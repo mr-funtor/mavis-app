a918ba49d2c5944fc6a6590399d589c0cb436e03
Olufunto Oyewole <funti009@gmail.com>
2022-04-05 22:52:22 +0100

    created the string of all the letters

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ const randomLetters= document.querySelector('.random-letters');
 const typedLetters= document.querySelector('.typed-letters');
 
 document.addEventListener('keydown',typingWord);
+
+const allLetters="abcdefghijklmnopqrstuvwxyz".split("");
